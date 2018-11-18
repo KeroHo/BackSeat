ae04394925ac0ed1c2c90e68069c4313f336be2c
Hieu Ho <hieuho99@gmail.com>
2018-11-18 08:47:22 -0800

    add Dashboard with random data generator in Utils.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import { render } from "react-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
+// import { Plugins } from "@capacitor/core";
+// const { Geolocation } = Plugins;
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+import { makeData, Logo, Tips } from "./Utils";
+var moment = require("moment");
 
 class Login extends Component {
   constructor(props) {
@@ -19,7 +26,7 @@ class Login extends Component {
 
     //   }
     // })
-    this.props.history.push("/next-view");
+    this.props.history.push("/dashboard");
   }
   render() {
     return (
@@ -50,11 +57,157 @@ class Login extends Component {
   }
 }
 
+class Dashboard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: makeData()
+    };
+  }
+  render() {
+    const { data } = this.state;
+    const cmake = "Toy";
+    const cmodel = "X Type";
+    const cyear = "2019";
+    const cmileage = "654";
+
+    return (
+      <div>
+        <br />
+        <h2>Car Information:</h2>
+        <p>
+          Make: {cmake} | Model: {cmodel} | Year: {cyear} <br />
+          vin:1GHBA3216511 | Mileage: {cmileage}
+        </p>
+        <br />
+
+        <ReactTable
+          data={data}
+          columns={[
+            {
+              id: "updatedAt",
+              Header: "Date (MM-DD-YYYY)",
+              render: props => (
+                <span>{moment.utc(props.value).format("MM-DD-YYYY")}</span>
+              )
+              // accessor: d => {
+              //   return Moment(d.updated_at)
+              //     .local()
+              //     .format("MM-DD-YYYY hh:mm:ss a");
+              // }
+            },
+            // {
+            //   Header: "Name",
+            //   columns: [
+            //     {
+            //       Header: "First Name",
+            //       accessor: "firstName"
+            //     },
+            //     {
+            //       Header: "Last Name",
+            //       id: "lastName",
+            //       accessor: d => d.lastName
+            //     },
+            {
+              Header: "Speed (Mph)",
+              accessor: "speed"
+              // Cell: row => (
+              //   <span
+              //     style={{
+              //       color:
+              //         row.value > 75
+              //           ? "#ff2e00"
+              //           : row.value > 65
+              //           ? "#ffbf00"
+              //           : "#57d500",
+              //       transition: "all .3s ease"
+              //     }}
+              //   >
+              //     &#x25cf;
+              //   </span>
+              // )
+            },
+
+            {
+              Header: "Info",
+              columns: [
+                {
+                  Header: "Profile Progress",
+                  accessor: "speed",
+                  Cell: row => (
+                    <div
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        backgroundColor: "#dadada",
+                        borderRadius: "2px"
+                      }}
+                    >
+                      <div
+                        style={{
+                          width: `${row.value}%`,
+                          height: "100%",
+                          backgroundColor:
+                            row.value < 65
+                              ? "#85cc00"
+                              : row.value < 80
+                              ? "#ffbf00"
+                              : "#ff2e00",
+                          borderRadius: "2px",
+                          transition: "all .2s ease-out"
+                        }}
+                      />
+                    </div>
+                  )
+                },
+                {
+                  Header: "Status",
+                  accessor: "status",
+                  Cell: row => (
+                    <span>
+                      <span
+                        style={{
+                          color:
+                            row.value === "dangerous"
+                              ? "#ff2e00"
+                              : row.value === "warning"
+                              ? "#ffbf00"
+                              : "#57d500",
+                          transition: "all .3s ease"
+                        }}
+                      >
+                        &#x25cf;
+                      </span>{" "}
+                      {row.value === "dangerous"
+                        ? "You got a ticket"
+                        : row.value === "warning"
+                        ? `First Warning`
+                        : "Your cool"}
+                    </span>
+                  )
+                }
+              ]
+            }
+          ]}
+          defaultPageSize={10}
+          className="-striped -highlight"
+        />
+        <br />
+        <Logo />
+      </div>
+    );
+  }
+}
+//setInterval(render(<Dashboard />, document.getElementById("root")), 1000);
+
+render(<App />, document.getElementById("root"));
+
 function App(props) {
   return (
     <BrowserRouter>
       <div>
         <Route path="/login" component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
       </div>
     </BrowserRouter>
   );
